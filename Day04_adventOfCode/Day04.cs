5a56d03138203e2cc879f324ea97275de20b2200
Felten Vanballenberghe <felten.vanballenberghe@geosolutions.be>
2023-12-12 08:16:52 +0100

    initialisation

---
+++ Day04_adventOfCode/Day04.cs
@@ -0,0 +1,7 @@
+﻿using Helpers;
+
+Console.WriteLine("Hello, AdventOfCode \n\n");
+List<string> lineList = new List<string>();
+lineList = InputTools.ReadAllLines("dayThree");
+List<string> SymbolList = new List<string>();
+
